6d5e71736965c77ce9395f515541ae017e3469ae
Renyu Wang <Renyu-Wang@users.noreply.github.com>
2020-05-02 15:52:12 -0700

    new version of q1b (5/2/2020)
    1.use binary mat and vec;
    2.use input parameter class: parser;
    3.use rank function;
    4use makefile.

---
+++ q1_Rotated_surface_codes/b_v2/q1b.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<tuple>             //for multiple returens of a function
+#include <itpp/itbase.h>    //for vectors and matrixes
+#include <chrono>           //for showing run-time
+#include "q1b_func.hpp"     //separation of functions from main file
+//#include <itpp/base/gf2mat.h> //for binary matrix
+
+//compile with g++ -std=c++11 `itpp-config --cflags` q1b_func.cpp q1b.cpp -o q1b `itpp-config --libs`
+
+int main(int argc, char **argv){
+    itpp::Parser parser;
+    parser.init(argc,argv);
+    int a = 1, b = 2, n = 6;
+    parser.get(a,"a");
+    parser.get(b,"b");
+    parser.get(n,"n");
+
+    int r = 0;
+    //std::tie(a, b, n) = input();
+    //start recording calculation time
+    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+
+    //input value of a, b, n and check if they fit the conditions
+    if (2*a+b >= n && n > 30){
+        std::cout << "Violate condition (2a+b < n) or (n <= 30).\n";
+        
+        std::tie(a, b, n) = input();
+    }
+    //find the number of indipendent generators
+    else{
+        //std::cout << "The gs are: " << write_gs(a, b, n) << "\n";
+        itpp::GF2mat gr(n,2*n);
+        gr = write_gs(a, b, n);
+        //r = num_indep(a, b, n, gr);
+        r = gr.row_rank() + 1;
+        std::cout << "There are " << r << " Independent generaters." << "\n";
+        std::cout << "There is(are) " << n-r << " encoded qubit(s)." << "\n";
+    }
+    //end recording calculation time
+    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+    //show run-time
+    std::chrono::duration<double, std::milli> time_span = (end - start)/1000;
+    std::cout << "Run-time " << time_span.count() << " seconds.\n";
+}
---
+++ q1_Rotated_surface_codes/b_v2/q1b_func.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+#include<tuple>
+#include <itpp/itbase.h>
+//#include <itpp/base/gf2mat.h>
+
+//input value of a, b, n
+std::tuple<int, int, int> input(){
+
+    int a2, b2, n2;
+    std::cout << "Enter the value of a = \n";
+    std::cin >> a2;
+    std::cout << "Enter the value of b = \n";
+    std::cin >> b2;
+    std::cout << "Enter the value of n = \n";
+    std::cin >> n2;
+
+    return std::make_tuple(a2, b2, n2);
+} 
+
+//find the first n gs
+itpp::GF2mat write_gs(int a3,int b3, int n3){
+
+    itpp::GF2mat gr(n3,2*n3);
+    itpp::bvec g(2*n3);
+
+    g.zeros();
+    //gr.zeros();
+
+    for (int i = 0; i < n3; i++){
+        g(i%n3) = 1;
+        g((i+2*a3+b3)%n3) = 1;
+        g((i+a3)%n3+n3) = 1;
+        g((i+a3+b3)%n3+n3) = 1;
+
+        gr.set_row(i,g);
+        //std::cout << "g = " << g.left(n) << "|" << g.right(n) << "\n";
+        g.zeros();
+    }
+
+    return gr;
+}
+
+//find the number of indipendent generators
+double num_indep(int a4, int b4, int n4, itpp::GF2mat gr2){
+
+    int m = n4;
+    int c = 0;
+    itpp::bvec g(2*n4);
+
+    for (int i = 0; i < m; i++){ 
+
+        for (int j = 0; j < m; j++){ 
+
+            if (i != j){
+
+                c = 0;
+                g.zeros();
+
+                for (int k = 0; k < 2*n4; k++){
+                
+                    g(k) = ( (int)gr2(i,k) + (int)gr2(j,k) ) % 2;
+                
+                }               
+
+                for (int l = 0; l < m; l++){
+
+                    if (g == gr2.get_row(l)){
+                        c = c + 1;
+                    }
+     
+                } 
+
+                if (c == 0){
+                    m = m + 1;
+                    gr2.set_size(m, 2*n4, true);
+                    gr2.set_row(m-1,g);
+                }
+            }
+            
+        }
+    }
+
+    return log2(m+1); 
+
+}
